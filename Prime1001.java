97193ad0bb3a577048873d28be43475b02d0ba90
Ashok Rao <ashok.usa@gmail.com>
2014-09-07 14:59:29 -0500

    comments

--- Prime1001.java
+++ Prime1001.java
@@ -1,5 +1,13 @@
 
 public class Prime1001 {
+	
+	/*
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+
+*/
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- SumPrimes.java
+++ SumPrimes.java
@@ -1,6 +1,9 @@
 
 public class SumPrimes {
 
+	/*The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		long sum =2;
